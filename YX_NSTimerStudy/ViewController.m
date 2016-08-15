082f8e6c829449d8ce32a9355c717bc80281f6c1
populary <410445919@qq.com>
2016-08-15 13:16:11 +0800

    change

--- YX_NSTimerStudy/ViewController.m
+++ YX_NSTimerStudy/ViewController.m
@@ -11,6 +11,9 @@
 @interface ViewController ()
 {
     NSTimer *timer;
+    NSInteger index;
+    NSArray *arr;
+    
 }
 @end
 
@@ -19,12 +22,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    //每隔s秒执行一次nslSome方法
-    timer = [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(nslSome) userInfo:nil repeats:NO];
+    //每隔s秒执行一次nslSome方法,如果repeats为NO则只执行一次
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(nslSome) userInfo:nil repeats:YES];
+    index = 0;
+    arr = @[@"麻",@"烦",@"给",@"个",@"星",@"星",@"!"];
 }
 -(void)nslSome{
     
-    NSLog(@"nslSome");
+    
+    //让该方法在执行6次后停止
+    index++;
+    UILabel *lab = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 80, 50)];
+    lab.text = [arr objectAtIndex:index-1];
+    lab.center = CGPointMake(self.view.center.x-400, 20+80*index);
+    lab.transform = CGAffineTransformMakeRotation(M_PI);
+    lab.backgroundColor = [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1];
+    lab.textAlignment = NSTextAlignmentCenter;
+    lab.alpha = 0.1;
+    [self.view addSubview:lab];
+    
+    [UIView animateWithDuration:1 delay:1 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        lab.center = CGPointMake(self.view.center.x, 20+80*index);
+    } completion:^(BOOL finished) {
+        lab.alpha = 1;
+        
+    }];
+    
+    [UIView animateWithDuration:2 delay:2 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        lab.transform = CGAffineTransformMakeRotation(0);
+    } completion:^(BOOL finished) {
+        
+    }];
+    
+    [UIView animateWithDuration:8 delay:8 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        lab.center = CGPointMake(self.view.center.x+400, 20+50*index);
+    } completion:^(BOOL finished) {
+        lab.alpha = 0.0;
+    }];
+    
+    [UIView animateWithDuration:12 delay:12 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        //lab.text = [arrTest objectAtIndex:index-1];
+        lab.center = CGPointMake(self.view.center.x, 20+80*index);
+    } completion:^(BOOL finished) {
+        lab.alpha = 1;
+        
+    }];
+
+    if (index == 6) {
+        [timer invalidate];
+    }
     
 }
 - (void)didReceiveMemoryWarning {
